957960a17d3c24764314396dcd28b851cbdc63f4
shiriperciger <shiri.perciger@gmail.com>
2023-05-15 16:04:28 -0400

    Finished Gatsby Tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,16 +2,17 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Us">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>Hi there! Im the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
 }
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="About Me" />
 
 // Step 3: Export your component
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -15,7 +16,7 @@ const IndexPage = () => {
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 // Step 3: Export your component
 export default IndexPage
